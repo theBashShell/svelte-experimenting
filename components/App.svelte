5ef8f8cb8be765769d1ed9f565ff22261c5412bb
bruno edoh <edohbruno@gmail.com>
2019-12-21 23:45:51 +0000

    each

---
+++ components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Button from "./Button.svelte";
+  let list = [0, 1, 23, 3];
+</script>
+
+<style>
+  div {
+    height: 20px;
+    background: wheat;
+    margin: 20px;
+  }
+</style>
+
+<main>
+	{#each list as val, i }
+<div>{i} --- {val}</div>
+{/each}
+</main>
