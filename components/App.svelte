6d46edecdd26c024fc97e2bec76585cda3d94c66
bruno edoh <edohbruno@gmail.com>
2019-12-22 23:01:26 +0000

    svelte:self

--- components/App.svelte
+++ components/App.svelte
@@ -1,29 +1,7 @@
-<script>
-  import { createEventDispatcher } from "svelte";
+<script> 
   import Button from "./Button.svelte";
-  let list = [0, 1, 23, 3];
-import {theme} from '../store.js'
-
-let t = ''
-  let dispatch = createEventDispatcher();
-
-const unsubscribe = theme.subscribe(a => t = a)
+  const count = 10
 </script>
 
-<style>
-  div {
-    height: 20px;
-    background: wheat;
-    margin: 20px;
-  }
-</style>
 
-<main>
-	{#each list as val, i }
-<div>{i} --- {val}</div>
-{/each}
-</main>
-
-<Button on:message={(event) => console.log(event.detail.text)} />
-<h1>{console.log(theme)}</h1>
-<h1>{t}</h1>
+<Button {count}   />
--- components/Button.svelte
+++ components/Button.svelte
@@ -1,27 +1,9 @@
 <script>
-	let count = 0;
-	import { createEventDispatcher } from "svelte";
-
-	let dispatch = createEventDispatcher();
-
-	function handleClick() {
-	  count += 1;
-	  dispatch("message", {
-	    text: "hello"
-	  });
-	}
+	export let count;  
 </script>
 
-<style>
-	button {
-	  background: #644236;
-	  color: white;
-	  border: none;
-	  padding: 8px 12px;
-	  border-radius: 2px;
-	}
-</style>
+{#if count === 10 }
+<svelte:self count={--count} />
+{/if} 
 
-<button on:click={handleClick}>
-  Clicked {count} {count === 1 ? 'time' : 'times'}
-</button>
+<h1>{count}</h1>
